d7baa4c96ad57bf676a200f3ccf7bb503bb2c9f6
Aaron Brady <aaron@insom.me.uk>
2024-04-09 16:09:15 -0400

    It compiles; but it isn't good

--- src/admin.rs
+++ src/admin.rs
@@ -1,38 +1,34 @@
-use std::fmt::Display;
-use std::fmt;
-
 #[allow(unused)]
 use diesel::prelude::*;
 use rocket::http::Status;
-use rocket::{get, request::FromRequest, request::Request};
+use rocket::outcome::IntoOutcome;
 use rocket::request;
+use rocket::{get, request::FromRequest, request::Request};
 use rocket_dyn_templates::{context, Template};
 
 pub struct AdminUser {}
 
 #[derive(Debug)]
 pub enum AppError {
-    MissingQuery,
-    MissingFilename,
-    ConfigLoad,
-}
-impl Display for AppError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "(POTATO)")
-    }
+    WhoAreYou,
+    InternalError,
 }
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for AdminUser {
     type Error = AppError;
 
-    async fn from_request(_req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        request::Outcome::Error((Status::NotFound, AppError::MissingQuery))
+    async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+        req.cookies()
+            .get("admin")
+            .and_then(|cookie| cookie.value().parse::<usize>().ok())
+            .and_then(|_| Some(AdminUser {}))
+            .or_error((Status::Forbidden, AppError::WhoAreYou))
     }
 }
 
 #[get("/admin")]
 pub fn index(_admin: AdminUser) -> Template {
-    let v = Vec::<bool>::new(); // an empty vec needs to have type information so it can be serailized #smh
-    Template::render("index", context! {weeks: &v})
+    let v = Vec::<bool>::new();
+    Template::render("index", context! {weeks: &v, admin: true})
 }
--- src/public.rs
+++ src/public.rs
@@ -6,5 +6,5 @@ use rocket_dyn_templates::{context, Template};
 #[get("/")]
 pub fn index() -> Template {
     let v = Vec::<bool>::new(); // an empty vec needs to have type information so it can be serailized #smh
-    Template::render("index", context! {weeks: &v})
+    Template::render("index", context! {weeks: &v, admin: false})
 }
