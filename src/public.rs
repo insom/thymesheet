494da3b027e4fe0b95f9bf1accbe3263f04697a4
Aaron Brady <aaron@insom.me.uk>
2024-04-24 16:31:00 -0400

    yes, this is more my style

--- src/public.rs
+++ src/public.rs
@@ -1,8 +1,7 @@
-use crate::{Thymesheet};
-use rocket_db_pools::Connection;
-use rocket_db_pools::sqlx::{Row, query};
+use crate::Thymesheet;
 use rocket::get;
 use rocket::response::status::NotFound;
+use rocket_db_pools::Connection;
 use rocket_dyn_templates::{context, Template};
 
 #[derive(sqlx::FromRow, serde::Serialize)]
@@ -14,22 +13,38 @@ pub struct Week {
 #[get("/")]
 pub async fn index(mut db: Connection<Thymesheet>) -> Template {
     let mut results: Vec<Week> = Vec::new();
-    let w: Week = sqlx::query_as("SELECT id, body FROM weeks").fetch_one(&mut **db).await.unwrap();
-    results.push(w);
+    let res: Result<Vec<Week>, _> = sqlx::query_as("SELECT id, body FROM weeks")
+        .fetch_all(&mut **db)
+        .await;
 
+    match res {
+        Err(_) => { /* nothing happens. */ }
+        Ok(mut w) => {
+            results.append(&mut w)
+        }
+    }
     Template::render("index", context! {weeks: &results, admin: false})
 }
 
 #[get("/week/<week>")]
-pub async fn week(week: i32) -> Result<Template, NotFound<String>> {
-    let results: Vec<bool> = Vec::new();
+pub async fn week(week: i32, mut db: Connection<Thymesheet>) -> Result<Template, NotFound<String>> {
+    let mut results: Vec<Week> = Vec::new();
+    let res = sqlx::query_as("SELECT id, body FROM weeks WHERE id = ?")
+        .bind(week)
+        .fetch_one(&mut **db)
+        .await;
 
-    if results.len() == 0 {
-        return Err(NotFound("Week Not Found".to_string()));
+    match res {
+        Err(_) => {
+            return Err(NotFound("Week Not Found".to_string()));
+        }
+        Ok(w) => {
+            results.push(w);
+
+            Ok(Template::render(
+                "index",
+                context! {weeks: &results, admin: false, title: format!("Week {}", week)},
+            ))
+        }
     }
-
-    Ok(Template::render(
-        "index",
-        context! {weeks: &results, admin: false, title: format!("Week {}", week)},
-    ))
 }
